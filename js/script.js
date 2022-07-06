64756719dd1f9e27a27f0af35ce3223a85602ab3
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-06 16:39:30 +0200

    fix splice

--- js/script.js
+++ js/script.js
@@ -14,11 +14,21 @@ const app = new Vue ({
                 text: 'Fare il bucato',
                 done: false
             }
-        ]
+        ],
+        newItem:''
     },
     methods: {
         removeItem(index){
             this.todos.splice(index,1)
+        },
+        changeStatus(index){
+
+            this.todos[index].done = !this.todos[index].done
+        },
+        addItem(userInput){
+            console.log(userInput)
+            this.todos.push(userInput).text
+            newItem = ''
         }
     },
 

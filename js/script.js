d11575eac06b6c96140f0d14d79f69d392e02d9e
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-06 14:39:39 +0200

    mile1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const app = new Vue ({
+    el: '#app',
+    data:{
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ]
+    }
+
+})
